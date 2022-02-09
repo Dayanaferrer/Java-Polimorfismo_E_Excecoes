e2ec587fab964d5cca0e17599a4e95dc0bc97baf
Dayana Ferreira <53124488+Dayanaferrer@users.noreply.github.com>
2022-02-09 09:25:41 -0300

    First commit

---
+++ bytebank-herdado/src/Funcionario.java
@@ -0,0 +1,37 @@
+
+public class Funcionario {
+	private String nome;
+	private String cpf;
+	private double salario;
+	
+	
+	// Contrutor (sempre tem o nome da classe) e diferente de um metodo ele não retorna nada.
+	public Funcionario() {
+		
+	}
+	
+	public double getBonificacao() {
+		return this.salario * 0.1;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}	
+	
+	
+}
