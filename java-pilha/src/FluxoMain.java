c9624fdfba7ec8997fd0cdf6c36fef4eab6e748a
Dayana Ferreira <53124488+Dayanaferrer@users.noreply.github.com>
2022-02-11 17:57:02 -0300

    ArithmeticExceptio

--- java-pilha/src/FluxoMain.java
+++ java-pilha/src/FluxoMain.java
@@ -16,6 +16,14 @@ public class FluxoMain {
         System.out.println("Inicio do metodo 2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+             
+            try {
+            	int a= i /0;
+            } catch (ArithmeticException ex) {
+            String msg = ex.getMessage();	
+            	System.out.println("ArithmeticExceptio" + msg);
+            	
+            }
         }
         System.out.println("Fim do metodo 2");
     }
