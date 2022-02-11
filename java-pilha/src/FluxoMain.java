4563836cc9c8e7b07754ffa11082c251153ad593
Dayana Ferreira <53124488+Dayanaferrer@users.noreply.github.com>
2022-02-10 21:34:20 -0300

    initial commit

---
+++ java-pilha/src/FluxoMain.java
@@ -0,0 +1,24 @@
+
+public class FluxoMain {
+	
+	public static void main(String[] args) {
+        System.out.println("Inicio do main");
+        metodo1();
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Inicio do metodo 1");
+        metodo2();
+        System.out.println("Fim do metodo 1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Inicio do metodo 2");
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Fim do metodo 2");
+    }
+
+}
