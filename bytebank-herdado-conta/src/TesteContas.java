747589d7ce561bf39b1fcf4e2692bb86282d996d
Dayana Ferreira <53124488+Dayanaferrer@users.noreply.github.com>
2022-02-11 21:49:47 -0300

    Finally

--- bytebank-herdado-conta/src/TesteContas.java
+++ bytebank-herdado-conta/src/TesteContas.java
@@ -1,7 +1,7 @@
 
 public class TesteContas {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SaldoInsuficienteException{
 		
 		ContaCorrente cc = new ContaCorrente(011, 111);
 		cc.deposita(100.0);
@@ -10,6 +10,7 @@ public class TesteContas {
 		cp.deposita(200.00);
 		
 		cc.transfere(10.0, cp);
+		
 		System.out.println("Saldo Conta Corrente: " + cc.getSaldo());
 		System.out.println("\nSaldo Conta Poupança: " + cp.getSaldo());
 	}
